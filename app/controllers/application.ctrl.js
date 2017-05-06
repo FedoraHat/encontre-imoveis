88b8454f2e6fbda2ead29dc800bbd55b4a0f1d8b
Damis Garcia <damisgarcia@gmail.com>
2017-05-06 16:49:12 -0300

    Step 01

---
+++ app/controllers/application.ctrl.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var ApplicationCtrl = function(){
+  var self = {
+    pesquisa: ''
+  }
+
+  return self
+}
+
+ApplicationCtrl.$inject = []
+
+angular.module("encontreImoveisApp").controller("ApplicationCtrl", ApplicationCtrl)
